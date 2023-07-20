8f7c40423c37448d85f1b952da2b3aa8f16163b8
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-07-19 21:12:32 -0500

    🔥 Always use ISR for BABYSTEPPING (#26035)

--- Marlin/src/module/temperature.cpp
+++ Marlin/src/module/temperature.cpp
@@ -160,10 +160,6 @@
   #include "stepper.h"
 #endif
 
-#if ENABLED(BABYSTEPPING) && DISABLED(INTEGRATED_BABYSTEPPING)
-  #include "../feature/babystep.h"
-#endif
-
 #if ENABLED(FILAMENT_WIDTH_SENSOR)
   #include "../feature/filwidth.h"
 #endif
@@ -4132,10 +4128,6 @@ void Temperature::isr() {
   //
   // Additional ~1kHz Tasks
   //
-
-  #if ENABLED(BABYSTEPPING) && DISABLED(INTEGRATED_BABYSTEPPING)
-    babystep.task();
-  #endif
 
   // Check fan tachometers
   TERN_(HAS_FANCHECK, fan_check.update_tachometers());
