fdd7fae632152703a958ee9fcd57a99b8ce0478e
Marcio T <mlt4356-github@yahoo.com>
2023-10-14 22:54:54 -0600

    ✨ Z_PROBE_ERROR_TOLERANCE (expose) (#26229)
    Co-authored-by: Scott Lahteine <thinkyhead@users.noreply.github.com>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1668,7 +1668,8 @@
  */
 #define Z_CLEARANCE_DEPLOY_PROBE   10 // (mm) Z Clearance for Deploy/Stow
 #define Z_CLEARANCE_BETWEEN_PROBES  5 // (mm) Z Clearance between probe points
-#define Z_CLEARANCE_MULTI_PROBE    10 // (mm) Z Clearance between multiple probes
+#define Z_CLEARANCE_MULTI_PROBE     10 // (mm) Z Clearance between multiple probes
+#define Z_PROBE_ERROR_TOLERANCE     3 // (mm) Tolerance for early trigger (<= -probe.offset.z + ZPET)
 //#define Z_AFTER_PROBING           5 // (mm) Z position after probing is done
 
 #define Z_PROBE_LOW_POINT          -8 // (mm) Farthest distance below the trigger-point to go before stopping
