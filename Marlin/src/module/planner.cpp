753c3d4d3ab380ba60939f4f054c30e97e653d6c
tombrazier <68918209+tombrazier@users.noreply.github.com>
2023-11-04 04:12:33 +0000

    🐛 Fix Backlash Compensation layer shift (#26392)

--- Marlin/src/module/planner.cpp
+++ Marlin/src/module/planner.cpp
@@ -2221,7 +2221,7 @@ bool Planner::_populate_block(
      * A correction function is permitted to add steps to an axis, it
      * should *never* remove steps!
      */
-    TERN_(BACKLASH_COMPENSATION, backlash.add_correction_steps(da, db, dc, dm, block));
+    TERN_(BACKLASH_COMPENSATION, backlash.add_correction_steps(dist, dm, block));
   }
 
   TERN_(HAS_EXTRUDERS, block->steps.e = esteps);
