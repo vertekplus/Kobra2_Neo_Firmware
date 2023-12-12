8ff03332ba8cbf8db01134b6fb464f2e00963e80
ellensp <530024+ellensp@users.noreply.github.com>
2023-12-12 14:30:28 +1300

    🐛 Fix MKS TS35 with BTT SKR 1.3/1.4 (#26176)

--- Marlin/src/HAL/LPC1768/tft/tft_spi.cpp
+++ Marlin/src/HAL/LPC1768/tft/tft_spi.cpp
@@ -53,11 +53,19 @@ void TFT_SPI::DataTransferBegin(uint16_t DataSize) {
   WRITE(TFT_CS_PIN, LOW);
 }
 
-uint32_t TFT_SPI::GetID() {
+#ifdef TFT_DEFAULT_DRIVER
+  #include "../../../lcd/tft_io/tft_ids.h"
+#endif
+
+uint32_t TFT_SPI::getID() {
   uint32_t id;
   id = ReadID(LCD_READ_ID);
   if ((id & 0xFFFF) == 0 || (id & 0xFFFF) == 0xFFFF)
-    id = ReadID(LCD_READ_ID4);
+    id = readID(LCD_READ_ID4);
+    #ifdef TFT_DEFAULT_DRIVER
+      if ((id & 0xFFFF) == 0 || (id & 0xFFFF) == 0xFFFF)
+        id = TFT_DEFAULT_DRIVER;
+    #endif
   return id;
 }
 
