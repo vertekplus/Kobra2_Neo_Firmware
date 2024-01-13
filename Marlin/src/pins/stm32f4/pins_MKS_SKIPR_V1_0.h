a0df67a3cf4c86415c67a57ba9c54cddad495e24
Robherc <68039049+robherc@users.noreply.github.com>
2024-01-12 23:03:34 -0500

    🔧 Fix SD connection defaults (#26666)
    Co-authored-by: thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
    Co-authored-by: Scott Lahteine <thinkyhead@users.noreply.github.com>

--- Marlin/src/pins/stm32f4/pins_MKS_SKIPR_V1_0.h
+++ Marlin/src/pins/stm32f4/pins_MKS_SKIPR_V1_0.h
@@ -267,7 +267,11 @@
 //
 #if ENABLED(SDSUPPORT)
   #ifndef SDCARD_CONNECTION
-    #define SDCARD_CONNECTION                LCD
+    #if ENABLED(NO_LCD_SDCARD)
+      #define SDCARD_CONNECTION          ONBOARD
+    #else
+      #define SDCARD_CONNECTION              LCD
+    #endif
   #endif
   #if SD_CONNECTION_IS(ONBOARD)
     //#define SOFTWARE_SPI
