8c02dead26c580479a6573e45331ce796a2b18cd
ejhoness <72996067+ejhoness@users.noreply.github.com>
2024-01-25 20:37:35 -0300

    ✏️ Fix draw_dialog.cpp typo (#26684)

--- Marlin/src/lcd/extui/mks_ui/draw_dialog.cpp
+++ Marlin/src/lcd/extui/mks_ui/draw_dialog.cpp
@@ -391,28 +391,12 @@ void lv_draw_dialog(uint8_t type) {
         lv_obj_align(labelDialog, nullptr, LV_ALIGN_CENTER, 0, -20);
       }
       else if (upload_result == 3) {
-        char buf[200];
-        int _index = 0;
-
-        strcpy_P(buf, PSTR(DIALOG_UPLOAD_FINISH_EN));
-        _index = strlen(buf);
-        buf[_index++] = '\n';
-        strcat_P(buf, PSTR(DIALOG_UPLOAD_SIZE_EN));
-
-        _index = strlen(buf);
-        buf[_index++] = ':';
-        sprintf_P(&buf[_index], PSTR(" %d KBytes\n"), (int)(upload_size / 1024));
-
-        strcat_P(buf, PSTR(DIALOG_UPLOAD_TIME_EN));
-        _index = strlen(buf);
-        buf[_index++] = ':';
-        sprintf_P(&buf[_index], PSTR(" %d s\n"), (int)upload_time_sec);
-
-        strcat_P(buf, PSTR(DIALOG_UPLOAD_SPEED_EN));
-        _index = strlen(buf);
-        buf[_index++] = ':';
-        sprintf_P(&buf[_index], PSTR(" %d KBytes/s\n"), (int)(upload_size / upload_time_sec / 1024));
-
+        MString<200> buf(
+          F(DIALOG_UPLOAD_FINISH_EN), '\n',
+          F(DIALOG_UPLOAD_SIZE_EN), F(": "), int(upload_size / 1024), F(" KBytes\n"),
+          F(DIALOG_UPLOAD_TIME_EN), F(": "), int(upload_time_sec), F(" s\n"),
+          F(DIALOG_UPLOAD_SPEED_EN), F(": "), int(upload_size / upload_time_sec / 1024), F(" KBytes/s\n")
+        );
         lv_label_set_text(labelDialog, buf);
         lv_obj_align(labelDialog, nullptr, LV_ALIGN_CENTER, 0, -20);
       }
