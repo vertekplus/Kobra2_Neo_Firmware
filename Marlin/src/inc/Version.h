8f5e6c6940c73764725abb86a0049705a49db33d
thinkyhead <thinkyhead@users.noreply.github.com>
2024-01-31 06:06:35 +0000

    [cron] Bump distribution date (2024-01-31)

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "1.0.2"
+  #define SHORT_BUILD_VERSION "bugfix-2.1.x"
 #endif
 
 /**
@@ -42,7 +42,7 @@
  * version was tagged.
  */
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2024-01-28"
+  #define STRING_DISTRIBUTION_DATE "2024-01-31"
 #endif
 
 /**
