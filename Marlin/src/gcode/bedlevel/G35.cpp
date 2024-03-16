4f3a686f8fecbe0c24ac1da30d75fd9b238363c9
Keith Bennett <13375512+thisiskeithb@users.noreply.github.com>
2024-03-16 12:56:33 -0700

    🩹 Fix G35 "Probing Failed" output (#26780)

--- Marlin/src/gcode/bedlevel/G35.cpp
+++ Marlin/src/gcode/bedlevel/G35.cpp
@@ -101,10 +101,12 @@ void GcodeSuite::G35() {
   LOOP_L_N(i, G35_PROBE_COUNT) {
     const float z_probed_height = probe.probe_at_point(tramming_points[i], PROBE_PT_RAISE, 0, true);
     if (isnan(z_probed_height)) {
-      SERIAL_ECHOPGM("G35 failed at point ", i + 1, " (");
-      SERIAL_ECHOPGM_P((char *)pgm_read_ptr(&tramming_point_name[i]));
-      SERIAL_CHAR(')');
-      SERIAL_ECHOLNPGM_P(SP_X_STR, tramming_points[i].x, SP_Y_STR, tramming_points[i].y);
+      SERIAL_ECHOLN(
+        F("G35 failed at point "), i + 1,
+        F(" ("), FPSTR(pgm_read_ptr(&tramming_point_name[i])), C(')'),
+        FPSTR(SP_X_STR), tramming_points[i].x,
+        FPSTR(SP_Y_STR), tramming_points[i].y
+      );
       err_break = true;
       break;
     }
