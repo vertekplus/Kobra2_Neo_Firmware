d3b8633a6be90580c640d19f6a96938129892dfa
ellensp <530024+ellensp@users.noreply.github.com>
2024-05-20 11:51:52 +1200

    ✨ SHOW_CUSTOM_BOOTSCREEN for HD44780 (#26793)
    Co-authored-by: Scott Lahteine <thinkyhead@users.noreply.github.com>

--- Marlin/src/lcd/marlinui.h
+++ Marlin/src/lcd/marlinui.h
@@ -115,7 +115,8 @@ enum LCDLeveingState:uint8_t {
   enum HD44780CharSet : uint8_t {
     CHARSET_MENU,
     CHARSET_INFO,
-    CHARSET_BOOT
+    CHARSET_BOOT,
+    CHARSET_BOOT_CUSTOM
   };
 #endif
 
