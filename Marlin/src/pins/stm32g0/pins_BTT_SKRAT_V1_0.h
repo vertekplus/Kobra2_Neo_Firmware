224494709175aef59cf487ae114fe6a7f2a22622
Keith Bennett <13375512+thisiskeithb@users.noreply.github.com>
2025-01-14 10:42:06 -0800

    🔧 BTT SKRat UART driver support pending (#27639)

--- Marlin/src/pins/stm32g0/pins_BTT_SKRAT_V1_0.h
+++ Marlin/src/pins/stm32g0/pins_BTT_SKRAT_V1_0.h
@@ -309,25 +309,18 @@
   //
   // Software serial
   //
-  #define X_SERIAL_TX_PIN                   PF10
-  #define X_SERIAL_RX_PIN        X_SERIAL_TX_PIN
-
-  #define Y_SERIAL_TX_PIN                   PD4
-  #define Y_SERIAL_RX_PIN        Y_SERIAL_TX_PIN
-
-  #define Z_SERIAL_TX_PIN                   PC8
-  #define Z_SERIAL_RX_PIN        Z_SERIAL_TX_PIN
-
-  #define E0_SERIAL_TX_PIN                  PD8
-  #define E0_SERIAL_RX_PIN      E0_SERIAL_TX_PIN
-
-  #define E1_SERIAL_TX_PIN                  PB11
-  #define E1_SERIAL_RX_PIN      E1_SERIAL_TX_PIN
+  //#define X_SERIAL_TX_PIN                 PF10
+  //#define Y_SERIAL_TX_PIN                 PD4
+  //#define Z_SERIAL_TX_PIN                 PC8
+  //#define E0_SERIAL_TX_PIN                PD8
+  //#define E1_SERIAL_TX_PIN                PB11
 
   // Reduce baud rate to improve software serial reliability
-  #ifndef TMC_BAUD_RATE
-    #define TMC_BAUD_RATE                  19200
-  #endif
+  //#ifndef TMC_BAUD_RATE
+  //  #define TMC_BAUD_RATE                19200
+  //#endif
+
+  #error "UART-based drivers are not supported on this board."
 
 #endif
 
@@ -415,7 +408,6 @@
     #define E2_CS_PIN                EXP1_06_PIN
     #if HAS_TMC_UART
       #define E2_SERIAL_TX_PIN       EXP1_06_PIN
-      #define E2_SERIAL_RX_PIN       EXP1_06_PIN
     #endif
   #endif
 
@@ -428,7 +420,6 @@
     #define E3_CS_PIN                EXP1_04_PIN
     #if HAS_TMC_UART
       #define E3_SERIAL_TX_PIN       EXP1_04_PIN
-      #define E3_SERIAL_RX_PIN       EXP1_04_PIN
     #endif
   #else
     #define E3_ENABLE_PIN            EXP2_07_PIN
@@ -443,7 +434,6 @@
     #define E4_CS_PIN                EXP1_02_PIN
     #if HAS_TMC_UART
       #define E4_SERIAL_TX_PIN       EXP1_02_PIN
-      #define E4_SERIAL_RX_PIN       EXP1_02_PIN
     #endif
   #else
     #define E4_ENABLE_PIN            EXP2_07_PIN
